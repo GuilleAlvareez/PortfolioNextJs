f8667cd1201787510956b994e3a87fb11fb96eca
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-08-16 21:33:16 +0200

    Limpieza

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import NavBar from '@/components/layout/NavBar'
 import Hero from '@/components/sections/Hero'
-// import About from '@/components/sections/About'
-// import Projects from '@/components/sections/Projects'
-// import Experience from '@/components/sections/Experience'
-import Labs from '@/components/sections/Labs'
-// import Contact from '@/components/sections/Contact'
 import SectionWrapper from '@/components/ui/SectionWrapper'
-// import Skills from '@/components/sections/Skills'
 import dynamic from 'next/dynamic';
 
 const About = dynamic(() => import('@/components/sections/About'));
