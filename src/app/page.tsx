c02f2d51bb0bb629778dab1626527a56a994a906
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-08-16 21:27:50 +0200

    Optimizacion de la pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import NavBar from '@/components/layout/NavBar'
 import Hero from '@/components/sections/Hero'
-import About from '@/components/sections/About'
-import Projects from '@/components/sections/Projects'
-import Experience from '@/components/sections/Experience'
+// import About from '@/components/sections/About'
+// import Projects from '@/components/sections/Projects'
+// import Experience from '@/components/sections/Experience'
 import Labs from '@/components/sections/Labs'
-import Contact from '@/components/sections/Contact'
+// import Contact from '@/components/sections/Contact'
 import SectionWrapper from '@/components/ui/SectionWrapper'
-import Skills from '@/components/sections/Skills'
+// import Skills from '@/components/sections/Skills'
+import dynamic from 'next/dynamic';
+
+const About = dynamic(() => import('@/components/sections/About'));
+const Projects = dynamic(() => import('@/components/sections/Projects'));
+const Experience = dynamic(() => import('@/components/sections/Experience'));
+const Skills = dynamic(() => import('@/components/sections/Skills'));
+const Contact = dynamic(() => import('@/components/sections/Contact'));
 
 export default function Home() {
   return (
