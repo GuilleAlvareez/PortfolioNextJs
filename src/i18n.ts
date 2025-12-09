6c7adeeffc27edda165c8ed5948b874e1eca5296
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-12-09 17:16:54 +0100

    feat: Add i18n configuration using next-intl for locale management and translation loading.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,16 @@
- import { notFound } from 'next/navigation';
-import { getRequestConfig } from 'next-intl/server';
+import { notFound } from "next/navigation";
+import { getRequestConfig } from "next-intl/server";
 
 // Can be imported from a shared config
-export const locales = ['es', 'en'] as const;
-export const defaultLocale = 'es' as const;
+export const locales = ["es", "en"] as const;
+export const defaultLocale = "es" as const;
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    locale: locale as string,
+    messages: (await import(`../public/translations/${locale}.json`)).default,
   };
 });
